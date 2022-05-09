c9c5e1be0936c3ca87093605d60981b82f7c3036
KnoxSamuel <knoxsa@oregonstate.edu>
2022-05-09 15:24:12 -0700

    error route updates

--- server.js
+++ server.js
@@ -14,29 +14,8 @@ const { generatePassword } = require('./PasswordGen')
 app.use(express.json());
 
 
-app.use(function (req, res, next) {
-    console.log("== Request received")
-    console.log("  - METHOD:", req.method)
-    console.log("  - URL:", req.url)
-    console.log("  - HEADERS:", req.headers)
-    next()
-});
-
-
-
 // default endpoint - password gen
 app.get('/', function (req, res, next) {
-
-    /*function getRandPw(len){
-        
-        var charset = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-        var randomness = '';
-
-        for (var i = 0; i < len; ++i) {
-            randomness += charset.charAt(Math.random() * charset.length);
-        };
-        return randomness;
-    };*/
 
     const pw = generatePassword(10); // default 10 chars, no pw len specified in url
     res.status(200).json({pw});
@@ -54,21 +33,16 @@ app.get('/:pwlen', function (req, res, next) {
         res.status(200).json({pw})
     } else {
         res.status(404).send({
-            err: "This URL was not recognized: " + req.originalUrl
+            err: "Password length: " + req.originalUrl + " is out-of-bounds (1-64 inclusive)"
         });
     };
 });
 
 
 
+// 404 route
 app.use('*', function (req, res, next) {
     res.status(404).send({
         err: "This URL was not recognized: " + req.originalUrl
     })
-});
-
-
-
-app.listen(port, function () {
-    console.log("== Server is listening on port:", port)
 });
