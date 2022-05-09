a08db878ccae46fbe15401abbb37fac378e49689
KnoxSamuel <knoxsa@oregonstate.edu>
2022-05-09 15:17:52 -0700

    small server fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80
 
-const { generatePassword } = require('../PasswordGen')
+const { generatePassword } = require('./PasswordGen')
 
 app.use(express.json());
 
